78637e95231e18c2106de6e433d11b60c46c38cf
darryl west <darryl.west@RainCitySoftware.com>
2014-03-22 07:50:35 -0700

    initial project

---
+++ code-generation/app/controllers/ApplicationFactory.js
@@ -0,0 +1,22 @@
+/**
+ *
+ *
+ * @author: darryl.west@roundpeg.com
+ * @created: 3/22/14 7:06 AM
+ */
+
+var app = '../../app',
+    services = app + '/services',
+    delegates = app + '/delegates';
+
+var AbstractApplicationFactory = require( 'node-commons' ).controllers.AbstractApplicationFactory,
+    BootStrap = require( 'node-commons' ).controllers.CommonBootStrap,
+    dash = require('lodash');
+
+var ApplicationFactory = function(options) {
+    'use strict';
+
+    var factory = this;
+
+
+};
---
+++ code-generation/test/controllers/ApplicationFactoryTests.js
@@ -0,0 +1,25 @@
+/**
+ *
+ *
+ * @author: darryl.west@roundpeg.com
+ * @created: 3/22/14 7:07 AM
+ */
+var should = require('chai').should();
+
+describe('ApplicationFactory', function() {
+    'use strict';
+
+    var createOptions = function() {
+        var opts = {}; // Config.test()
+
+        // logging...
+
+        return opts;
+    };
+
+    describe('#instance', function() {
+        it('should create an instance of ApplicationFactory', function() {
+
+        });
+    });
+});
