b1253531bf2ddd2ee58a3c58c3fd5b0c88880af0
darryl west <darryl.west@RainCitySoftware.com>
2014-03-23 09:43:59 -0700

    added factory tests

--- code-generation/test/controllers/ApplicationFactoryTests.js
+++ code-generation/test/controllers/ApplicationFactoryTests.js
@@ -55,5 +55,27 @@ describe('ApplicationFactory', function() {
         it('should have all expected methods by size', function() {
             dash.methods( factory ).length.should.equal( methods.length );
         });
+
+        it('should execute all known methods', function() {
+            var obj;
+
+            methods.forEach(function(method) {
+                if (method === 'createRoutePath') {
+                    obj = factory.createRoutePath('this', 'that');
+                    should.exist( obj );
+                } else if (method.indexOf('create') === 0) {
+                    obj = factory[ method ]();
+                    should.exist( obj );
+                } else if (method.indexOf('get') === 0) {
+                    factory[ method ]();
+                }
+            });
+        });
+    });
+
+    describe('#Configuration', function() {
+        it('should create configuration dao');
+        it('should create configuration data service');
+        it('should create configuration web service');
     });
 });
